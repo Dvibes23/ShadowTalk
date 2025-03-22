46ab8ff192f00a134e5199b0917ccd5c377dd738
Damola Olasupo <162465413+Dvibes23@users.noreply.github.com>
2025-03-23 00:13:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,100 +1,101 @@
-
-const express = require("express");
-const http = require("http");
-const socketIO = require("socket.io");
-const path = require("path");
-
+const express = require('express');
 const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path');
+
+const rooms = {};
+const publicRooms = new Set();
+const logs = {};
+const reports = [];
 
 app.use(express.static(__dirname));
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+app.get('/admin', (req, res) => res.sendFile(__dirname + '/admin.html'));
 
-let rooms = {};
-let logs = {};
-
-io.on("connection", (socket) => {
-  socket.on("requestRoom", (data, callback) => {
-    let room = data.roomInput;
-    if (!room) {
-      const available = Object.entries(rooms).filter(([_, info]) => info.public && info.users.length > 0);
-      room = available.length ? available[Math.floor(Math.random() * available.length)][0] : "room-" + Math.floor(Math.random() * 9999);
+io.on('connection', socket => {
+  socket.on('join', ({ nickname, room, isPublic, password }) => {
+    if (!rooms[room]) rooms[room] = { users: {}, public: isPublic, password: password || '' };
+    if (rooms[room].password && rooms[room].password !== password) {
+      socket.emit('message', '<em>Incorrect room password!</em>');
+      return;
     }
 
-    if (!rooms[room]) {
-      rooms[room] = { public: data.isPublic, users: [], nicknames: {}, logs: [] };
-    }
-
-    callback(room);
-  });
-
-  socket.on("join", ({ room, name }) => {
     socket.join(room);
     socket.room = room;
-    socket.nickname = name;
-    rooms[room].users.push(socket.id);
-    rooms[room].nicknames[socket.id] = name;
-    logs[room] = logs[room] || [];
-    logs[room].push(`${name} joined`);
+    socket.nickname = nickname;
+    rooms[room].users[socket.id] = nickname;
+    if (isPublic) publicRooms.add(room);
 
-    io.to(room).emit("userList", Object.values(rooms[room].nicknames));
-    io.to(room).emit("message", `${name} joined the room.`);
+    io.to(room).emit('message', `Welcome ${nickname}!`);
+    io.to(room).emit('users', Object.values(rooms[room].users));
+    saveLog(room, `${nickname} joined.`)
   });
 
-  socket.on("message", ({ room, name, text }) => {
-    const msg = `${name}: ${text}`;
-    logs[room]?.push(msg);
-    io.to(room).emit("message", msg);
+  socket.on('message', ({ message, room, nickname }) => {
+    const formatted = `${nickname}: ${message}`;
+    io.to(room).emit('message', formatted);
+    saveLog(room, formatted);
   });
 
-  socket.on("typing", ({ room, name }) => {
-    socket.to(room).emit("typing", name);
+  socket.on('voice', ({ blob, sender, room }) => {
+    io.to(room).emit('voice', { blob, sender });
+    saveLog(room, `${sender} sent a voice note`);
   });
 
-  socket.on("audio", (data) => {
-    logs[data.room]?.push(`${data.sender} sent a voice note`);
-    io.to(data.room).emit("audio", data);
+  socket.on('typing', ({ nickname, room }) => {
+    socket.to(room).emit('typing', nickname);
   });
 
-  socket.on("disconnect", () => {
+  socket.on('report', ({ reportedUser, reason, room }) => {
+    reports.push({ reportedUser, reason, room, time: new Date().toISOString() });
+  });
+
+  socket.on('kick', ({ target, room }) => {
+    const targetId = Object.keys(rooms[room].users).find(id => rooms[room].users[id] === target);
+    if (targetId) {
+      io.to(targetId).emit('message', 'You have been kicked by the admin.');
+      io.sockets.sockets.get(targetId).disconnect();
+    }
+  });
+
+  socket.on('mute', ({ target, room }) => {
+    const targetId = Object.keys(rooms[room].users).find(id => rooms[room].users[id] === target);
+    if (targetId) io.to(targetId).emit('message', 'You have been muted by the admin.');
+  });
+
+  socket.on('getRooms', () => {
+    socket.emit('roomList', Array.from(publicRooms));
+  });
+
+  socket.on('getUsers', room => {
+    const userList = rooms[room] ? Object.values(rooms[room].users) : [];
+    socket.emit('userList', { room, users: userList });
+  });
+
+  socket.on('getLogs', room => {
+    socket.emit('logs', logs[room] || []);
+  });
+
+  socket.on('getReports', () => {
+    socket.emit('reports', reports);
+  });
+
+  socket.on('disconnect', () => {
     const room = socket.room;
-    const name = socket.nickname;
     if (room && rooms[room]) {
-      delete rooms[room].nicknames[socket.id];
-      rooms[room].users = rooms[room].users.filter(id => id !== socket.id);
-      logs[room]?.push(`${name} left`);
-      io.to(room).emit("message", `${name} left the room.`);
-      io.to(room).emit("userList", Object.values(rooms[room].nicknames));
-    }
-  });
-
-  // Admin actions
-  socket.on("admin-login", (password, cb) => {
-    cb(password === "shadowadmin");
-  });
-
-  socket.on("admin-rooms", () => {
-    socket.emit("admin-rooms", Object.keys(rooms));
-  });
-
-  socket.on("admin-users", (room) => {
-    const users = rooms[room]?.nicknames || {};
-    socket.emit("admin-users", users);
-  });
-
-  socket.on("admin-logs", (room) => {
-    socket.emit("admin-logs", logs[room] || []);
-  });
-
-  socket.on("admin-kick", ({ room, target }) => {
-    const id = Object.keys(rooms[room]?.nicknames || {}).find(key => rooms[room].nicknames[key] === target);
-    if (id) {
-      io.to(id).emit("message", "You were kicked by admin.");
-      io.sockets.sockets.get(id)?.disconnect();
+      const nickname = rooms[room].users[socket.id];
+      delete rooms[room].users[socket.id];
+      io.to(room).emit('message', `${nickname} left the room.`);
+      io.to(room).emit('users', Object.values(rooms[room].users));
+      saveLog(room, `${nickname} left.`);
     }
   });
 });
 
-server.listen(3000, () => {
-  console.log("ShadowTalk server running at http://localhost:3000");
-});
+function saveLog(room, msg) {
+  if (!logs[room]) logs[room] = [];
+  logs[room].push(`[${new Date().toLocaleTimeString()}] ${msg}`);
+}
+
+http.listen(3000, () => console.log('ShadowTalk server running on http://localhost:3000'));
